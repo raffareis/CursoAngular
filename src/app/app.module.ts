dd6566bd1d5a56747a9feeb41edf5be9ea4d9b41
raffareis <raffareis@gmail.com>
2022-03-02 05:41:00 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { GenericListComponent } from './utilities/generic-list/generic-list.component';
+import { MenuComponent } from './menu/menu.component';
+import { RatingComponent } from './utilities/rating/rating.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MoviesListComponent,
+    GenericListComponent,
+    MenuComponent,
+    RatingComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.css']
+})
+export class MoviesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  deleteMovie(id: number): void {
+    this.movies?.splice(this.movies.findIndex(movie => movie.id === id), 1);
+    console.log("deletando filme: " + id);
+  }
+
+  @Input()
+  movies: { id: number; name: string; year: number; rating: number; genre: string; description: string; image: string; }[] | undefined;
+
+
+}
